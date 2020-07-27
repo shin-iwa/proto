c70459520167ef1ffe9f349a454a009789c622f0
shin-iwa <luckystar0443@gmail.com>
2020-07-27 15:29:23 +0900

    Revert "add"
    This reverts commit dead3745a85ca548d37557a42b7a346a7fd516ed.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,6 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
-  has_rich_text :content
   
   def self.search(search)
     return Tweet.all unless search
