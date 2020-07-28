906fd24937087558a21a36fd60257ce7ecfc9052
shin-iwa <luckystar0443@gmail.com>
2020-07-28 13:33:52 +0900

    restart

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :email, :encrypted_password, presence: true
+  validates :image, presence: true, allow_blank: true
   has_many :tweets
   has_many :comments
+  mount_uploader :icon, ImageUploader
 end
--- db/migrate/20200627063645_devise_create_users.rb
+++ db/migrate/20200627063645_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname, null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :icon
 
       ## Recoverable
       t.string   :reset_password_token
