8eb46c76b44956666295d09469ad1a0e010032d8
shin-iwa <luckystar0443@gmail.com>
2020-08-25 10:38:53 +0900

    fix

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index,except: [:index, :show, :search]
 
   def index
-    @tweets = Tweet.limit(10).includes(:user, :tags).order("created_at DESC")
+    @tweets = Tweet.limit(10).includes(:user).order("created_at DESC")
   end
 
   def new
