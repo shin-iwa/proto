93ec0ca150bcce25ffccdfffb6fb4a0d6b8b607c
shin-iwa <luckystar0443@gmail.com>
2020-08-25 11:05:29 +0900

    default

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,19 +4,10 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.limit(10).includes(:user).order("created_at DESC")
-    # @tags = Tweet.tag_counts_on(:tags).order('count DESC')
-
   end
 
   def new
     @tweet = Tweet.new
-    # @tweet.tag_list = "awesome, slick, hefty" # this should be familiar
-    # @tweet.save
-
-    # @tweet.tags # => [,,]
-
-    # @tweet.tags_count # => [,,]
-
   end
 
   def create
@@ -44,11 +35,6 @@ class TweetsController < ApplicationController
   def search
     @tweets = Tweet.search(params[:keyword])
   end
-
-  # def tag_cloud
-  #   # order('count DESC')でカウントの多い順にタグを並べています
-  #   @tags = Review.tag_counts_on(:tags).order('count DESC')
-  # end
 
   private
   def tweet_params
