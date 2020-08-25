61767bb766e96fae0952a0c975e4ba21091052dc
shin-iwa <luckystar0443@gmail.com>
2020-08-25 21:31:13 +0900

    comp article roots

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,52 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show,]
+  # before_action :move_to_index,except: [:index, :show]
+
   def index
-    article = Article.all
-    # @nickname = user.nickname
-    @tweets = Tweet.limit(10).includes(:user).order("created_at DESC")
+    @articles = Article.limit(10).includes(:user).order("created_at DESC")
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(artrcle_params)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(tweet_params)
+  end
+
+  def show
+    @article = Article.new
+    @articles = @article.tweets.includes(:user)
+  end
+
+  # def search
+  #   @tweets = Tweet.search(params[:keyword])
+  # end
+
+  private
+  def article_params
+    params.require(:article).permit( :title, :body).merge(user_id: current_user.id)
+  end
+
+  def set_article
+    @artilce = Article.find(params[:id])
+  end
+
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in? 
+  # end
 end
+
