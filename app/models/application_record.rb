6c67c25642d37d6087f298498fe22076cb566433
shin-iwa <luckystar0443@gmail.com>
2020-09-01 12:14:28 +0900

    befire adding pagenate

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def previous
-    Novel.where("id < ?", self.id).order("id DESC").first
-  end
+  # def previous
+  #   Novel.where("id < ?", self.id).order("id DESC").first
+  # end
 
-  def next
-    Novel.where("id > ?", self.id).order("id ASC").first
-  end
+  # def next
+  #   Novel.where("id > ?", self.id).order("id ASC").first
+  # end
 
   # def previous
   #   Article.where("id < ?", self.id).order("id DESC").first
