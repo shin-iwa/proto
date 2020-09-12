8b30c54360240014ceb0ba51d1dde0c140f3b4b6
shin-iwa <luckystar0443@gmail.com>
2020-09-12 19:56:47 +0900

    update scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'tweets#index'
   root to: 'articles#index'
   resources :tweets do
     resources :comments, only: :create
@@ -8,8 +7,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :users, only: [:show] do
-  end
+  resources :users, only: [:show] 
   resources :articles do
     resources :novels, only: [:show, :create, :destroy, :edit, :update]
   end
