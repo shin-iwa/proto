a00893a368096acad05a2c99c7151572b89f1ca1
shin-iwa <luckystar0443@gmail.com>
2020-09-12 22:22:02 +0900

    fix search

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,10 @@ class Article < ApplicationRecord
   accepts_nested_attributes_for :novels
   
   validates :title, :body, presence: true
+
+  def self.search(search)
+    return Article.all unless search
+    Article.where('title LIKE(?)', "%#{search}%")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
+  resources :users, only: [:show] 
   resources :tweets do
     resources :comments, only: :create
     collection do
       get 'search'
     end
   end
-  resources :users, only: [:show] 
   resources :articles do
     resources :novels, only: [:show, :create, :destroy, :edit, :update]
+    collection do
+      get 'search'
+    end
   end
 end
