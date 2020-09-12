6712d1ba6682cd97068df9cd902197c2d22ed257
shin-iwa <luckystar0443@gmail.com>
2020-09-12 23:41:43 +0900

    add serch

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,12 +2,10 @@ class Tweet < ApplicationRecord
   validates :title,:text, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
-  # has_many :articles, dependent: :destroy
-  # has_rich_text :content
   
   def self.search(search)
     return Tweet.all unless search
-    Tweet.where('text LIKE(?)', "%#{search}%")
+    Tweet.where('title LIKE(?)', "%#{search}%")
   end
 
   mount_uploader :image, ImageUploader
