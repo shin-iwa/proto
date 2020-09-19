8f134b4526cc9739cb289db34d665e6204912fc3
shin-iwa <luckystar0443@gmail.com>
2020-09-19 23:59:25 +0900

    update detail page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,10 +13,10 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     if @tweet.save
-      redirect_to "/users/#{current_user.id}"
+      redirect_to "/home/#{current_user.id}"
       flash[:notice] = "投稿が完了しました"
     else
-      redirect_to "/users/#{current_user.id}"
+      redirect_to "/home/#{current_user.id}"
       flash[:alert] = "投稿に失敗しました"
     end
   end
