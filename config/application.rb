4f8701d5309a24f72c4a9640badd7ab983c16818
shin-iwa <luckystar0443@gmail.com>
2020-09-21 23:12:35 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Proto
     config.active_record.default_timezone = :local
 
     config.i18n.default_locale = :ja
-
+    config.assets.initialize_on_precompile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
